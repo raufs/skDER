ae5ee5c55cf9fcc6c47d1ad2c8a13257957f6095
raufs <salamzader@gmail.com>
2023-08-15 22:18:39 +0000

    update to v1.0.3

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 import os
 
 setup(name='skDER',
-      version='1.0.2',
+      version='1.0.3',
       description='Program to select distinct representatives from an input set of microbial genomes.',
       url='http://github.com/raufs/skDER/',
       author='Rauf Salamzade',
       author_email='salamzader@gmail.com',
       license='BSD-3',
       packages=['skDER'],
-      scripts=['skder'],
+      scripts=['bin/skder'],
       zip_safe=False)
 
 try:
