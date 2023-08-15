942caf1df4a7ae97e4f990f9d0064ece377205ed
Rauf Salamzade <salamzader@gmail.com>
2023-08-15 18:46:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import os
 
 setup(name='skDER',
-      version='1.0.3',
+      version='1.0.4',
       description='Program to select distinct representatives from an input set of microbial genomes.',
       url='http://github.com/raufs/skDER/',
       author='Rauf Salamzade',
